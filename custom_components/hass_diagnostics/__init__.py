8065de064c3a106bbf858419142f201dc7f68ca0
Alex X <alexey.khit@gmail.com>
2024-04-18 17:11:00 +0300

    Add options to config_flow

--- custom_components/hass_diagnostics/__init__.py
+++ custom_components/hass_diagnostics/__init__.py
@@ -8,3 +8,7 @@ PLATFORMS = ["sensor"]
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
--- custom_components/hass_diagnostics/config_flow.py
+++ custom_components/hass_diagnostics/config_flow.py
@@ -1,6 +1,8 @@
-from homeassistant.config_entries import ConfigFlow
+import voluptuous as vol
+from homeassistant.config_entries import ConfigEntry, ConfigFlow, OptionsFlow
+from homeassistant.core import callback
 
-from . import DOMAIN
+from .core.const import DOMAIN, SMART_LOG, START_TIME
 
 
 class FlowHandler(ConfigFlow, domain=DOMAIN):
@@ -10,5 +12,43 @@ class FlowHandler(ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input: dict = None):
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
-        name = getattr(self.hass.data["integrations"][DOMAIN], "name")
-        return self.async_create_entry(title=name, data={})
+
+        if user_input:
+            return self.async_create_entry(
+                title="Hass Diagnostics", data={}, options=user_input
+            )
+
+        return self.async_show_form(step_id="user", data_schema=data_schema())
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry: ConfigEntry):
+        return OptionsFlowHandler(config_entry)
+
+
+# noinspection PyUnusedLocal
+class OptionsFlowHandler(OptionsFlow):
+    def __init__(self, config_entry: ConfigEntry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input: dict = None):
+        if user_input:
+            self.hass.async_create_task(
+                self.hass.config_entries.async_reload(self.config_entry.entry_id)
+            )
+            return self.async_create_entry(title="", data=user_input)
+
+        data = data_schema(self.config_entry.options)
+        return self.async_show_form(step_id="init", data_schema=data)
+
+
+def data_schema(defaults=None) -> vol.Schema:
+    schema = {
+        vol.Required(SMART_LOG, default=True): bool,
+        vol.Required(START_TIME, default=True): bool,
+    }
+    if defaults:
+        for key in schema:
+            if (value := defaults.get(key.schema)) is not None:
+                key.default = vol.default_factory(value)
+    return vol.Schema(schema)
