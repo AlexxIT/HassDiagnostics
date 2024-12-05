4c4c74c39df61848a7f659bde0938abc8f5ddd7a
Alex X <alexey.khit@gmail.com>
2024-12-05 13:42:49 +0300

    Add auto reload after update options

--- custom_components/hass_diagnostics/__init__.py
+++ custom_components/hass_diagnostics/__init__.py
@@ -7,8 +7,16 @@ PLATFORMS = ["sensor"]
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+
+    if not config_entry.update_listeners:
+        config_entry.add_update_listener(async_update_options)
+
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
+
+
+async def async_update_options(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_reload(entry.entry_id)
